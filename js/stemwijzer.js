280a3e36c3cdc485383ca25e3166ab0f353acf32
Elise Eijkhout <99038787@mydavinci.nl>
2018-02-09 11:36:54 +0100

    Werkende buttons waaronder een aparte start button, geen onclick's in html pagina, goede en werkende css.

--- js/stemwijzer.js
+++ js/stemwijzer.js
@@ -1,25 +1,70 @@
 var nr = 0;
 
+var startBtn = document.getElementById('start');
 var subject = document.getElementById('subject');
 var statement = document.getElementById('statement');
-var next = document.getElementById('skip');
-var back = document.getElementById('prev');
-var eens = document.getElementById('eens');
-var oneens = document.getElementById('oneens');
+var nextBtn = document.getElementById('skip');
+var backBtn = document.getElementById('back');
+var eensBtn = document.getElementById('eens');
+var oneensBtn = document.getElementById('oneens');
+var geenBtn = document.getElementById('geen');
+var main = document.getElementById('main');
+
+startBtn.addEventListener("click", start);
+nextBtn.addEventListener("click", function() {
+	nr++;
+	subject.innerHTML = subjects[nr].title;
+	statement.innerHTML = subjects[nr].statement;	
+});
+
+eensBtn.addEventListener("click", function() {
+	nr++;
+	subject.innerHTML = subjects[nr].title;
+	statement.innerHTML = subjects[nr].statement;	
+});
+
+oneensBtn.addEventListener("click", function() {
+	nr++;
+	subject.innerHTML = subjects[nr].title;
+	statement.innerHTML = subjects[nr].statement;	
+});
+
+geenBtn.addEventListener("click", function() {
+	nr++;
+	subject.innerHTML = subjects[nr].title;
+	statement.innerHTML = subjects[nr].statement;	
+});
+
+backBtn.addEventListener("click", function() {
+	nr--;
+	subject.innerHTML = subjects[nr].title;
+	statement.innerHTML = subjects[nr].statement;	
+});
+
+
+
 
 subject.innerHTML = subjects[nr].title;
 statement.innerHTML = subjects[nr].statement;
 
-var next = function(event) {
-	nr++;
-	subject.innerHTML = subjects[nr].title;
-	statement.innerHTML = subjects[nr].statement;
-}
+main.style.display = "none";
 
-var prev = function(event){
-	nr--;
-	subject.innerHTML = subject[nr].title;
-	statement.innerHTML = subjects[nr].statement;
+
+function start() {
+ console.log("start()");
+
+var start = document.getElementById('start').style.display = "none";
+main.style.display = "block";
+
+
+ 	
+ var start = document.getElementById('start');
+ nr++;
+ start.innerHTML = 'start';
+ 
+
+
+
 }
 
 
